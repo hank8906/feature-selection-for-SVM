8422decd823098691d4d3419cf50e07996736b35
hankhuang <huanghank2000@gmail.com>
2024-03-22 23:37:08 +0800

    Information gain

---
+++ SVM/SVM_IG.py
@@ -0,0 +1,84 @@
+import numpy as np
+import pandas as pd
+from sklearn.svm import SVC
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.feature_selection import SelectKBest,mutual_info_classif
+from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
+
+# 加载数据
+data_csv_path = r"C:\Users\Hank\DS\dataset\Heart_disease_cleveland_new.csv"
+df = pd.read_csv(data_csv_path)
+
+# 分割特征和标签，假设最后一列是标签
+X = df.iloc[:, :-1].values
+y = df.iloc[:, -1].values
+
+# 分割数据为训练集和测试集
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# 標準化特徵
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# 计算每个特征的信息增益
+information_gains = mutual_info_classif(X_train_scaled, y_train)
+
+# 打印每个特征的信息增益
+# print("Information gains for each feature:")
+# for i, gain in enumerate(information_gains):
+#     print(f"Feature {i}: {gain}")
+
+# 选择信息增益大于等于阈值的特征
+threshold = 0.05  # 设置信息增益的阈值
+selected_features_indices = np.where(information_gains >= threshold)[0]
+selected_features = df.columns[:-1][selected_features_indices]
+
+print("Infromation gain :")
+# 打印选择的特征
+print(f"selected_features: {selected_features}")
+
+
+# 更新数据集，仅保留选择的特征
+X_train_selected = X_train_scaled[:, selected_features_indices]
+X_test_selected = X_test_scaled[:, selected_features_indices]
+
+# 创建支持向量机模型
+param_grid = [
+    {'kernel': ['rbf'], 'gamma': [1e-3, 1e-4], 'C': [1, 10, 100, 1000]},
+    {'kernel': ['linear'], 'C': [1, 10, 100, 1000]}
+]
+
+# 在选定的特征上训练和测试模型
+gscv = GridSearchCV(SVC(), param_grid, cv=5, scoring='accuracy', n_jobs=-1)
+gscv.fit(X_train_selected, y_train)
+
+# print(f"Best hyperparameters: {gscv.best_params_}")
+# print(f"Best cross-validation score: {gscv.best_score_:.2f}")
+
+best_model = gscv.best_estimator_
+test_accuracy = best_model.score(X_test_selected, y_test)
+
+# 使用选定的特征评估测试集
+y_pred = best_model.predict(X_test_selected)
+
+# 计算混淆矩阵
+conf_matrix = confusion_matrix(y_test, y_pred)
+
+# 计算敏感性
+sensitivity = recall_score(y_test, y_pred)
+
+# 计算特异性
+specificity = conf_matrix[0, 0] / (conf_matrix[0, 0] + conf_matrix[0, 1])
+
+# 计算F-measure
+f_measure = f1_score(y_test, y_pred)
+print(f"Test accuracy with best params: {test_accuracy:.2f}")
+print(f"Sensitivity: {sensitivity:.2f}")
+print(f"Specificity: {specificity:.2f}")
+print(f"F-measure: {f_measure:.2f}")
+
